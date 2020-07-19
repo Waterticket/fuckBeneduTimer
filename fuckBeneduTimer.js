1ce4c61ec15f50b194573497e5322041c29bc975
Waterticket <matthew218@naver.com>
2020-07-19 14:08:15 +0900

    Create fuckBeneduTimer.js

---
+++ fuckBeneduTimer.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         fuckBeneduTimer
+// @namespace    https://hoto.dev
+// @version      1.0
+// @description  Stop benedu timer
+// @author       Waterticket
+// @match        https://www.benedu.co.kr/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    clearInterval(logoutTimer);
+    // Disable Timer
+})();
